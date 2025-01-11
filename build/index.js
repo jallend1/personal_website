5277b2b07d0f3d09e98d186d924e2b673ed66ca3
Jason Allen <jallend1@gmail.com>
2025-01-11 15:08:10 -0800

    Begins implementing some logic for element details

--- build/index.js
+++ build/index.js
@@ -1,3 +1,28 @@
 "use strict";
 const readyToGo = true;
 console.log(readyToGo);
+const el = document.getElementById("about");
+if (el) {
+    console.log(el);
+}
+else {
+    console.error("Element with id 'about' not found.");
+}
+const sectionElements = document.querySelectorAll("section");
+console.log(sectionElements);
+console.dir(sectionElements[0]);
+sectionElements.forEach((el) => {
+    el.addEventListener("click", () => {
+        console.log(`Clicked on ${el.id}`);
+    });
+    el.addEventListener("mouseover", () => {
+        console.log(`Hovered over ${el.id}`);
+        el.classList.add("active-element");
+        console.log(`Height: ${el.clientHeight}`);
+        console.log(`Width: ${el.clientWidth}`);
+    });
+    el.addEventListener("mouseout", () => {
+        console.log(`Mouse out of ${el.id}`);
+        el.classList.remove("active-element");
+    });
+});
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,31 @@
 const readyToGo: boolean = true;
 console.log(readyToGo);
+
+const el: HTMLElement | null = document.getElementById("about");
+if (el) {
+  console.log(el);
+} else {
+  console.error("Element with id 'about' not found.");
+}
+
+const sectionElements: NodeListOf<HTMLElement> =
+  document.querySelectorAll("section");
+console.log(sectionElements);
+console.dir(sectionElements[0]);
+
+sectionElements.forEach((el: HTMLElement) => {
+  el.addEventListener("click", () => {
+    console.log(`Clicked on ${el.id}`);
+  });
+
+  el.addEventListener("mouseover", () => {
+    console.log(`Hovered over ${el.id}`);
+    el.classList.add("active-element");
+    console.log(`Height: ${el.clientHeight}`);
+    console.log(`Width: ${el.clientWidth}`);
+  });
+  el.addEventListener("mouseout", () => {
+    console.log(`Mouse out of ${el.id}`);
+    el.classList.remove("active-element");
+  });
+});
