5f185fa59b600667f9fecfd1045fb06d18d802ef
Jason Allen <jallend1@gmail.com>
2025-01-19 12:58:08 -0800

    Refactors to make code not melt my eyeballs off because of its disordered nature

--- build/index.js
+++ build/index.js
@@ -1,116 +1,117 @@
 "use strict";
-const elements = document.querySelectorAll("section, img, a, p, h1, h2, h3, h4, code, div, span, ul, li, hr, footer, header");
-const elementIds = [
-    "element-name",
-    "element-attributes",
-    "element-dimensions",
-    "element-text",
-    "element-grandparent",
-    "element-parent",
-    "element-children",
-    "element-siblings",
-];
 const resetElements = () => {
+    const elementIds = [
+        "element-name",
+        "element-attributes",
+        "element-dimensions",
+        "element-text",
+        "element-grandparent",
+        "element-parent",
+        "element-children",
+        "element-siblings",
+    ];
     elementIds.forEach((id) => {
         const element = document.querySelector(`#${id}`);
         if (element)
             element.textContent = "";
     });
 };
-elements.forEach((el) => {
-    el.addEventListener("mouseover", (e) => {
-        var _a, _b, _c, _d, _e, _f;
-        e.stopPropagation();
-        // Don't highlight elements inside the asides or the container element
-        if (el.closest("aside") || el.classList.contains("container"))
+const setTextContent = (id, text) => {
+    const element = document.querySelector(`#${id}`);
+    if (element)
+        element.textContent = text;
+};
+const populateAttributesList = (id, attributes) => {
+    const element = document.querySelector(`#${id}`);
+    if (element) {
+        element.innerHTML = "";
+        for (let i = 0; i < attributes.length; i++) {
+            const attr = attributes[i];
+            const attrEl = document.createElement("li");
+            attrEl.innerHTML = `<span class="attr-name">${attr.name}</span>: <span class="attr-value">${attr.value}</span>`;
+            element.appendChild(attrEl);
+        }
+    }
+};
+const populateChildNodes = (id, children) => {
+    const element = document.querySelector(`#${id}`);
+    if (element && children.length > 0) {
+        element.innerHTML = "";
+        const filteredChildren = Array.from(children).filter((child) => child.nodeName !== "#text");
+        if (filteredChildren.length === 0) {
+            const noChildren = document.createElement("li");
+            noChildren.textContent = "None";
+            element.appendChild(noChildren);
+        }
+        else {
+            const childrenCount = filteredChildren.reduce((acc, child) => {
+                acc[child.nodeName] = acc[child.nodeName]
+                    ? acc[child.nodeName] + 1
+                    : 1;
+                return acc;
+            }, {});
+            for (let key in childrenCount) {
+                const childCountEl = document.createElement("li");
+                childCountEl.textContent = `${key}: ${childrenCount[key]}`;
+                element.appendChild(childCountEl);
+            }
+        }
+    }
+};
+const populateSiblingNodes = (id, siblings) => {
+    const element = document.querySelector(`#${id}`);
+    if (element) {
+        element.innerHTML = "";
+        if (!siblings)
             return;
-        el.classList.add("active-element");
-        // TODO: Refactor to use a single function to populate all elements
-        const elementName = document.querySelector("#element-name");
-        if (elementName)
-            elementName.textContent = el.tagName;
-        const elementAttributes = document.querySelector("#element-attributes");
-        if (elementAttributes) {
-            elementAttributes.innerHTML = "";
-            for (let i = 0; i < el.attributes.length; i++) {
-                const attr = el.attributes[i];
-                const attrEl = document.createElement("li");
-                attrEl.innerHTML = `<span class="attr-name">${attr.name}</span>: <span class="attr-value">${attr.value}</span>`;
-                elementAttributes.appendChild(attrEl);
-            }
-            console.log(el);
-            console.dir(el);
+        const filteredSiblings = Array.from(siblings).filter((sibling) => sibling.nodeName !== "#text");
+        if (filteredSiblings.length === 0) {
+            const noSiblings = document.createElement("li");
+            noSiblings.textContent = "None";
+            element.appendChild(noSiblings);
         }
-        const elementDimensions = document.querySelector("#element-dimensions");
-        if (elementDimensions) {
-            const { width, height } = el.getBoundingClientRect();
-            elementDimensions.textContent = `width: ${Math.floor(width)}px, height: ${Math.floor(height)}px`;
-        }
-        const elementText = document.querySelector("#element-text");
-        if (elementText)
-            elementText.textContent = el.textContent
-                ? el.textContent.length.toString()
-                : "0";
-        const grandParentNode = document.querySelector("#element-grandparent");
-        if (grandParentNode)
-            grandParentNode.textContent =
-                (_c = (_b = (_a = el.parentElement) === null || _a === void 0 ? void 0 : _a.parentElement) === null || _b === void 0 ? void 0 : _b.tagName) !== null && _c !== void 0 ? _c : "";
-        const parentNode = document.querySelector("#element-parent");
-        if (parentNode)
-            parentNode.textContent = (_e = (_d = el.parentElement) === null || _d === void 0 ? void 0 : _d.tagName) !== null && _e !== void 0 ? _e : "";
-        const childNodes = document.querySelector("#element-children");
-        if (childNodes) {
-            childNodes.innerHTML = "";
-            const filteredChildren = Array.from(el.childNodes).filter((child) => child.nodeName !== "#text");
-            if (filteredChildren.length === 0) {
-                const noChildren = document.createElement("li");
-                noChildren.textContent = "None";
-                childNodes.appendChild(noChildren);
-            }
-            else {
-                const childrenCount = filteredChildren.reduce((acc, child) => {
-                    acc[child.nodeName] = acc[child.nodeName]
-                        ? acc[child.nodeName] + 1
-                        : 1;
-                    return acc;
-                }, {});
-                for (let key in childrenCount) {
-                    const childCountEl = document.createElement("li");
-                    childCountEl.textContent = `${key}: ${childrenCount[key]}`;
-                    childNodes.appendChild(childCountEl);
-                }
+        else {
+            const siblingCount = filteredSiblings.reduce((acc, sibling) => {
+                acc[sibling.nodeName] = acc[sibling.nodeName]
+                    ? acc[sibling.nodeName] + 1
+                    : 1;
+                return acc;
+            }, {});
+            for (let key in siblingCount) {
+                const siblingCountEl = document.createElement("li");
+                siblingCountEl.textContent = `${key}: ${siblingCount[key]}`;
+                element.appendChild(siblingCountEl);
             }
         }
-        const siblingNodes = document.querySelector("#element-siblings");
-        if (siblingNodes) {
-            siblingNodes.innerHTML = "";
-            const siblings = (_f = el.parentElement) === null || _f === void 0 ? void 0 : _f.children;
-            if (siblings) {
-                // Excludes the current element and #text from the sibling list
-                const filteredSiblings = Array.from(siblings)
-                    .filter((sibling) => sibling !== el)
-                    .filter((sibling) => sibling.nodeName !== "#text");
-                // Compiles count of each sibling type
-                const siblingCount = filteredSiblings.reduce((acc, sibling) => {
-                    acc[sibling.nodeName] = acc[sibling.nodeName]
-                        ? acc[sibling.nodeName] + 1
-                        : 1;
-                    return acc;
-                }, {});
-                for (let key in siblingCount) {
-                    const siblingCountEl = document.createElement("li");
-                    siblingCountEl.textContent = `${key}: ${siblingCount[key]}`;
-                    siblingNodes.appendChild(siblingCountEl);
-                }
-            }
-        }
+    }
+};
+const updateElements = (el) => {
+    var _a, _b, _c, _d, _e, _f, _g;
+    el.classList.add("active-element");
+    setTextContent("element-name", el.tagName);
+    setTextContent("element-grandparent", (_c = (_b = (_a = el.parentElement) === null || _a === void 0 ? void 0 : _a.parentElement) === null || _b === void 0 ? void 0 : _b.tagName) !== null && _c !== void 0 ? _c : "");
+    setTextContent("element-parent", (_e = (_d = el.parentElement) === null || _d === void 0 ? void 0 : _d.tagName) !== null && _e !== void 0 ? _e : "");
+    setTextContent("element-text", el.textContent ? el.textContent.length.toString() : "0");
+    const { width, height } = el.getBoundingClientRect();
+    setTextContent("element-dimensions", `width: ${Math.floor(width)}px, height: ${Math.floor(height)}px`);
+    populateAttributesList("element-attributes", el.attributes);
+    populateChildNodes("element-children", el.childNodes);
+    populateSiblingNodes("element-siblings", (_g = (_f = el.parentElement) === null || _f === void 0 ? void 0 : _f.children) !== null && _g !== void 0 ? _g : null);
+};
+const addEventListeners = () => {
+    const elements = document.querySelectorAll("section, img, a, p, h1, h2, h3, h4, code, div, span, ul, li, hr, footer, header");
+    elements.forEach((el) => {
+        el.addEventListener("mouseover", (e) => {
+            e.stopPropagation();
+            // Don't highlight elements inside the asides or the container element
+            if (el.closest("aside") || el.classList.contains("container"))
+                return;
+            updateElements(el);
+        });
+        el.addEventListener("mouseout", () => {
+            el.classList.remove("active-element");
+            resetElements();
+        });
     });
-    el.addEventListener("mouseout", () => {
-        el.classList.remove("active-element");
-        // TODO: Refactor to use a single function to clear all elements
-        // const elementName: HTMLElement | null =
-        //   document.querySelector("#element-name");
-        // if (elementName) elementName.textContent = "";
-        resetElements();
-    });
-});
+};
+addEventListeners();
--- src/index.ts
+++ src/index.ts
@@ -1,137 +1,140 @@
-const elements: NodeListOf<HTMLElement> = document.querySelectorAll(
-  "section, img, a, p, h1, h2, h3, h4, code, div, span, ul, li, hr, footer, header"
-);
-
-const elementIds = [
-  "element-name",
-  "element-attributes",
-  "element-dimensions",
-  "element-text",
-  "element-grandparent",
-  "element-parent",
-  "element-children",
-  "element-siblings",
-];
-
 const resetElements = () => {
+  const elementIds = [
+    "element-name",
+    "element-attributes",
+    "element-dimensions",
+    "element-text",
+    "element-grandparent",
+    "element-parent",
+    "element-children",
+    "element-siblings",
+  ];
   elementIds.forEach((id) => {
     const element = document.querySelector(`#${id}`);
     if (element) element.textContent = "";
   });
 };
 
-elements.forEach((el: HTMLElement) => {
-  el.addEventListener("mouseover", (e) => {
-    e.stopPropagation();
-    // Don't highlight elements inside the asides or the container element
-    if (el.closest("aside") || el.classList.contains("container")) return;
+const setTextContent = (id: string, text: string) => {
+  const element: HTMLElement | null = document.querySelector(`#${id}`);
+  if (element) element.textContent = text;
+};
 
-    el.classList.add("active-element");
-    // TODO: Refactor to use a single function to populate all elements
-    const elementName: HTMLElement | null =
-      document.querySelector("#element-name");
-    if (elementName) elementName.textContent = el.tagName;
-    const elementAttributes: HTMLElement | null = document.querySelector(
-      "#element-attributes"
-    );
-    if (elementAttributes) {
-      elementAttributes.innerHTML = "";
-      for (let i = 0; i < el.attributes.length; i++) {
-        const attr = el.attributes[i];
-        const attrEl = document.createElement("li");
-        attrEl.innerHTML = `<span class="attr-name">${attr.name}</span>: <span class="attr-value">${attr.value}</span>`;
-        elementAttributes.appendChild(attrEl);
-      }
-      console.log(el);
-      console.dir(el);
+const populateAttributesList = (id: string, attributes: NamedNodeMap) => {
+  const element: HTMLElement | null = document.querySelector(`#${id}`);
+  if (element) {
+    element.innerHTML = "";
+    for (let i = 0; i < attributes.length; i++) {
+      const attr = attributes[i];
+      const attrEl = document.createElement("li");
+      attrEl.innerHTML = `<span class="attr-name">${attr.name}</span>: <span class="attr-value">${attr.value}</span>`;
+      element.appendChild(attrEl);
     }
-    const elementDimensions: HTMLElement | null = document.querySelector(
-      "#element-dimensions"
-    );
-    if (elementDimensions) {
-      const { width, height } = el.getBoundingClientRect();
-      elementDimensions.textContent = `width: ${Math.floor(
-        width
-      )}px, height: ${Math.floor(height)}px`;
-    }
-    const elementText: HTMLElement | null =
-      document.querySelector("#element-text");
-    if (elementText)
-      elementText.textContent = el.textContent
-        ? el.textContent.length.toString()
-        : "0";
-    const grandParentNode: HTMLElement | null = document.querySelector(
-      "#element-grandparent"
-    );
-    if (grandParentNode)
-      grandParentNode.textContent =
-        el.parentElement?.parentElement?.tagName ?? "";
-    const parentNode: HTMLElement | null =
-      document.querySelector("#element-parent");
-    if (parentNode) parentNode.textContent = el.parentElement?.tagName ?? "";
-    const childNodes: HTMLElement | null =
-      document.querySelector("#element-children");
+  }
+};
 
-    if (childNodes) {
-      childNodes.innerHTML = "";
-      const filteredChildren = Array.from(el.childNodes).filter(
-        (child) => child.nodeName !== "#text"
+const populateChildNodes = (id: string, children: NodeList) => {
+  const element: HTMLElement | null = document.querySelector(`#${id}`);
+  if (element && children.length > 0) {
+    element.innerHTML = "";
+    const filteredChildren = Array.from(children).filter(
+      (child) => child.nodeName !== "#text"
+    );
+    if (filteredChildren.length === 0) {
+      const noChildren = document.createElement("li");
+      noChildren.textContent = "None";
+      element.appendChild(noChildren);
+    } else {
+      const childrenCount = filteredChildren.reduce(
+        (acc: { [key: string]: number }, child) => {
+          acc[child.nodeName] = acc[child.nodeName]
+            ? acc[child.nodeName] + 1
+            : 1;
+          return acc;
+        },
+        {}
       );
-      if (filteredChildren.length === 0) {
-        const noChildren = document.createElement("li");
-        noChildren.textContent = "None";
-        childNodes.appendChild(noChildren);
-      } else {
-        const childrenCount = filteredChildren.reduce(
-          (acc: { [key: string]: number }, child) => {
-            acc[child.nodeName] = acc[child.nodeName]
-              ? acc[child.nodeName] + 1
-              : 1;
-            return acc;
-          },
-          {}
-        );
-        for (let key in childrenCount) {
-          const childCountEl = document.createElement("li");
-          childCountEl.textContent = `${key}: ${childrenCount[key]}`;
-          childNodes.appendChild(childCountEl);
-        }
+      for (let key in childrenCount) {
+        const childCountEl = document.createElement("li");
+        childCountEl.textContent = `${key}: ${childrenCount[key]}`;
+        element.appendChild(childCountEl);
       }
     }
-    const siblingNodes: HTMLElement | null =
-      document.querySelector("#element-siblings");
-    if (siblingNodes) {
-      siblingNodes.innerHTML = "";
-      const siblings = el.parentElement?.children;
-      if (siblings) {
-        // Excludes the current element and #text from the sibling list
-        const filteredSiblings = Array.from(siblings)
-          .filter((sibling) => sibling !== el)
-          .filter((sibling) => sibling.nodeName !== "#text");
-        // Compiles count of each sibling type
-        const siblingCount = filteredSiblings.reduce(
-          (acc: { [key: string]: number }, sibling) => {
-            acc[sibling.nodeName] = acc[sibling.nodeName]
-              ? acc[sibling.nodeName] + 1
-              : 1;
-            return acc;
-          },
-          {}
-        );
-        for (let key in siblingCount) {
-          const siblingCountEl = document.createElement("li");
-          siblingCountEl.textContent = `${key}: ${siblingCount[key]}`;
-          siblingNodes.appendChild(siblingCountEl);
-        }
+  }
+};
+
+const populateSiblingNodes = (id: string, siblings: HTMLCollection | null) => {
+  const element: HTMLElement | null = document.querySelector(`#${id}`);
+  if (element) {
+    element.innerHTML = "";
+    if (!siblings) return;
+    const filteredSiblings = Array.from(siblings).filter(
+      (sibling) => sibling.nodeName !== "#text"
+    );
+    if (filteredSiblings.length === 0) {
+      const noSiblings = document.createElement("li");
+      noSiblings.textContent = "None";
+      element.appendChild(noSiblings);
+    } else {
+      const siblingCount = filteredSiblings.reduce(
+        (acc: { [key: string]: number }, sibling) => {
+          acc[sibling.nodeName] = acc[sibling.nodeName]
+            ? acc[sibling.nodeName] + 1
+            : 1;
+          return acc;
+        },
+        {}
+      );
+      for (let key in siblingCount) {
+        const siblingCountEl = document.createElement("li");
+        siblingCountEl.textContent = `${key}: ${siblingCount[key]}`;
+        element.appendChild(siblingCountEl);
       }
     }
+  }
+};
+
+const updateElements = (el: HTMLElement) => {
+  el.classList.add("active-element");
+
+  setTextContent("element-name", el.tagName);
+  setTextContent(
+    "element-grandparent",
+    el.parentElement?.parentElement?.tagName ?? ""
+  );
+  setTextContent("element-parent", el.parentElement?.tagName ?? "");
+  setTextContent(
+    "element-text",
+    el.textContent ? el.textContent.length.toString() : "0"
+  );
+  const { width, height } = el.getBoundingClientRect();
+  setTextContent(
+    "element-dimensions",
+    `width: ${Math.floor(width)}px, height: ${Math.floor(height)}px`
+  );
+  populateAttributesList("element-attributes", el.attributes);
+
+  populateChildNodes("element-children", el.childNodes);
+
+  populateSiblingNodes("element-siblings", el.parentElement?.children ?? null);
+};
+
+const addEventListeners = () => {
+  const elements: NodeListOf<HTMLElement> = document.querySelectorAll(
+    "section, img, a, p, h1, h2, h3, h4, code, div, span, ul, li, hr, footer, header"
+  );
+  elements.forEach((el: HTMLElement) => {
+    el.addEventListener("mouseover", (e) => {
+      e.stopPropagation();
+      // Don't highlight elements inside the asides or the container element
+      if (el.closest("aside") || el.classList.contains("container")) return;
+      updateElements(el);
+    });
+    el.addEventListener("mouseout", () => {
+      el.classList.remove("active-element");
+      resetElements();
+    });
   });
-  el.addEventListener("mouseout", () => {
-    el.classList.remove("active-element");
-    // TODO: Refactor to use a single function to clear all elements
-    // const elementName: HTMLElement | null =
-    //   document.querySelector("#element-name");
-    // if (elementName) elementName.textContent = "";
-    resetElements();
-  });
-});
+};
+
+addEventListeners();
